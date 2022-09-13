cbbd6026e4f8252391e5af3f07173f31b4ac4f61
Jijo Joseph <jijojosephk@hotmail.com>
2022-09-13 12:01:33 +0530

    Fixed issue: Not playing from second time.

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
-//const { NodeSound } = require('.');
-//const player = NodeSound.getDefaultPlayer();
+const { NodeSound } = require('.');
+const player = NodeSound.getDefaultPlayer();
+const samplefile = 'samples/mixkit-dry-pop-up-notification-alert-2356.wav';
+player.play(samplefile).then(() => {
+})
